a396354b1e8eae97a6662a64496d2bccdadfbfb3
USEREN <USEREN>
2017-01-24 15:00:04 +0100

    first commit with existing files

---
+++ lesson 1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+This section covers some of the most important datatypes and how we can assign values and read values from them
+"""
+from datetime import date
+""" to initialize or not to initialize ? """
+matrix_2by2 = []
+
+
+integer = 99
+string = 'poonacha'
+i_list = [1,2,3,4]
+c_list = ['p','po','poo','poon']
+dictionary = {'p' : 1 , 'po' : 2, 'poo' : 3, 'poon' : 4}
+date = date.today()
+
+""" lets play with these data types """
+""" First we will see how the output looks """
+
+print('This is an integer: ',integer )
+print('This is a string: ',string )
+print('This is an integer list: ',i_list )
+print('This is a char list: ',c_list )
+print('This is a dictionary: ',dictionary)
+print('This is today: ', date)
+
+""" Finding the data types """
+
+""" Editing these data types"""
