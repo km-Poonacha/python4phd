7b829eec99d073c1d33b303fb7d3e2669204ad28
Poonacha Medappa <km.poonacha@gmail.com>
2017-01-25 11:14:53 +0100

    set of commits marking the completion of the day

---
+++ Lesson 2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 22:10:23 2017
+
+@author: medapa
+
+Lets cover tweepy, pandas, numpy and matplotlib
+"""
+
--- lesson 1.py
+++ lesson 1.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 This section covers some of the most important datatypes and how we can assign values and read values from them
+
+we need to cover datetime, csv libraries 
 """
 from datetime import date
 """ to initialize or not to initialize ? """
@@ -24,19 +26,37 @@ print('This is a char list: ',c_list )
 print('This is a dictionary: ',dictionary)
 print('This is today: ', date)
 
-""" Finding the data types """
+""" Finding the data types. """
 print(type(date))
 
 """ Editing these data types"""
 
 #concatinating a string 
 print( string + 'km' )
-#can you repeat the same with date ?
+#data type conversion str() and int ()
 print(str(date) * 4)
 
-# using spli()
-
+# using split()
 string2 = input('Enter a string pls: ')
 print (string2)
 print (string2.split( ))
 
+# create a matrix
+
+# functions .. lets create a function to input a sentence and splits the sentence into words
+# understand local variables 
+# enter arguments into the functions and return from the function 
+
+def splitwords():
+    string2 = input('Enter a string pls: ')
+    words = string2.split( )
+    return words
+    
+def main():
+    l_words = splitwords()
+    
+
+
+    
+    
+main()
